fc2ac66569df5f46bfa36d78128367ba88735794
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-07-05 01:04:51 -0300

    Aulas

---
+++ aula4_S_responsabilidade_unica/inicial.py
@@ -0,0 +1,14 @@
+class SistemaCadastral:
+
+    def cadastrar(self, nome: str, idade: int) -> None:
+        if isinstance(nome, str) and isinstance(idade, int):
+            print("Acessando o banco de dados...")
+            print(f"Cadastrar usuario {nome}, idade {idade}")
+        else:
+            print("dados invalidos")
+
+
+
+
+# SOLID
+# S - Responsabilidade Unica / Single Responsability
---
+++ aula5_variaveis_de_classe/run.py
@@ -0,0 +1,9 @@
+class MinhaClasse:
+
+    estatico = "lhama"
+
+    def __init__(self, estado) -> None:
+        self.__estado = estado
+
+
+obj = MinhaClasse(True)
